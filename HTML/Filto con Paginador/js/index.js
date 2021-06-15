19feee48b5551d80e9d009887db36efe860edf4c
sebacairola <sebastiancairola@gmail.com>
2021-06-15 15:22:11 -0300

    Subo paginador

---
+++ HTML/Filto con Paginador/js/index.js
@@ -0,0 +1,382 @@
+const notas = [        
+    {   
+        "titulo": "Bulbasaur",
+        "subTitulo": "#001",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        "categorias": ["todos", "hierva"]
+    },
+    {   
+        "titulo": "Ivysaur",
+        "subTitulo": "#002",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
+        "categorias": ["todos", "hierva"]
+    },
+    {
+        "titulo": "Venusaur",
+        "subTitulo": "#003",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png",
+        "categorias": ["todos", "hierva"]
+    },
+    {
+        "titulo": "Charmander",
+        "subTitulo": "#004",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+        "categorias": ["todos", "fuego"]
+    },
+    {
+        "titulo": "Charmeleon",
+        "subTitulo": "#005",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png",
+        "categorias": ["todos", "fuego"]
+    },
+    {
+        "titulo": "Charizard",
+        "subTitulo": "#006",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png",
+        "categorias": ["todos", "fuego"]
+    },
+    {
+        "titulo": "Squirtle",
+        "subTitulo": "#007",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+        "categorias": ["todos", "agua"]
+    },
+    {
+        "titulo": "Wartortle",
+        "subTitulo": "#008",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/8.png",
+        "categorias": ["todos", "agua"]
+    },
+    {
+        "titulo": "Blastoise",
+        "subTitulo": "#009",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/9.png",
+        "categorias": ["todos", "agua"]
+    },
+    {
+        "titulo": "Caterpie",
+        "subTitulo": "#010",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/10.png",
+        "categorias": ["todos", "bicho"]
+    },
+    {
+        "titulo": "Metapod",
+        "subTitulo": "#011",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/11.png",
+        "categorias": ["todos", "bicho"]
+    },
+    {
+        "titulo": "Butterfree",
+        "subTitulo": "#012",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/12.png",
+        "categorias": ["todos", "bicho"]
+    },
+    {
+        "titulo": "Weedle",
+        "subTitulo": "#013",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/13.png",
+        "categorias": ["todos", "bicho"]
+    },
+    {
+        "titulo": "Kakuna",
+        "subTitulo": "#014",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/14.png",
+        "categorias": ["todos", "bicho"]
+    },
+    {
+        "titulo": "Beedrill",
+        "subTitulo": "#015",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/15.png",
+        "categorias": ["todos", "bicho"]
+    },
+    {
+        "titulo": "Pidgey",
+        "subTitulo": "#016",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/16.png",
+        "categorias": ["todos", "volador"]
+    },
+    {
+        "titulo": "Pidgeotto",
+        "subTitulo": "#017",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/17.png",
+        "categorias": ["todos", "volador"]
+    },
+    {
+        "titulo": "Pidgeot",
+        "subTitulo": "#018",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/18.png",
+        "categorias": ["todos", "volador"]
+    },
+    {
+        "titulo": "Rattata",
+        "subTitulo": "#19",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/19.png",
+        "categorias": ["todos", "normal"]
+    },
+    {
+        "titulo": "Raticate",
+        "subTitulo": "#20",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/20.png",
+        "categorias": ["todos", "normal"]
+    },
+    {
+        "titulo": "Spearow",
+        "subTitulo": "#21",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/21.png",
+        "categorias": ["todos", "volador"]
+    },
+    {
+        "titulo": "Fearow",
+        "subTitulo": "#22",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/22.png",
+        "categorias": ["todos", "volador"]
+    },
+    {
+        "titulo": "Ekans",
+        "subTitulo": "#23",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/23.png",
+        "categorias": ["todos", "veneno"]
+    },
+    {
+        "titulo": "Arbok",
+        "subTitulo": "#24",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/24.png",
+        "categorias": ["todos", "veneno"]
+    },
+    {
+        "titulo": "Pikachu",
+        "subTitulo": "#25",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+        "categorias": ["todos", "electrico"]
+    },
+    {
+        "titulo": "Raichu",
+        "subTitulo": "#26",
+        "textoEnlace": "Ver más",
+        "urlEnlace": "#",
+        "urlImagen": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/26.png",
+        "categorias": ["todos", "electrico"]
+    }
+
+];
+    
+const elementos = {
+    paginador: document.querySelector("[data-comp='paginador']"),
+    contendorNotas: document.querySelector("[data-comp='box-container']"),
+    notas: document.querySelector("[data-comp='articulo']"),
+    bttnContainer: document.querySelector("[data-comp='bttn-container']"),
+    arrowNext: document.querySelector("[data-comp='next']"),
+    arrowPrev: document.querySelector("[data-comp='prev']"),
+    bttnNumber: document.querySelector("[data-number]"),
+    filter: document.querySelector("#form_filtros"),
+    bttnFilter: document.querySelector("[data-comp='filter']"),
+    filterSeleccionado: document.querySelector("#form_filtros .seleccionada"),
+    filterSeleccionadoSpan: document.querySelector("#form_filtros .seleccionada span")
+}
+
+const DEFAULT_SELECTED_CATEGORY = 'todos';
+const CANT_ARTICLES_PER_PAGE = 9;
+
+let allElementsOfCategory;
+
+function init(){
+    setArticles(DEFAULT_SELECTED_CATEGORY);
+    renderPage(1);
+    renderPaginator();    
+    hideArrows(1);
+}
+
+function setArticles(categoria) {
+
+    let arrayContainer = [];
+
+    notas.forEach(function(articulo){
+
+        for (let categoriaArticulo of articulo.categorias){
+
+            if ( categoriaArticulo === categoria ){
+                arrayContainer.push(articulo);
+            }
+        }
+
+    });
+
+    allElementsOfCategory = arrayContainer;    
+}
+
+function renderPaginator(){
+
+    let cantElementsOfCategory = allElementsOfCategory.length;
+    let html = "";
+
+    if (cantElementsOfCategory <= CANT_ARTICLES_PER_PAGE) {
+        // hide Paginator
+        $(elementos.paginador).hide();
+    } else {
+        // Show Paginator
+        $(elementos.paginador).show();
+
+        let numberOfItems = Math.ceil(cantElementsOfCategory / CANT_ARTICLES_PER_PAGE) + 1;
+
+        for (var i = 1; i < numberOfItems; i++) {
+            if (i === 1) {
+                html += '<li><a href="#" data-number=' + i + ' class="active">' + i + '</a></li>';
+            } else {
+                html += '<li><a href="#" data-number=' + i + '>' + i + '</a></li>';
+            }
+
+            $(elementos.bttnContainer).html(html);
+        }
+    }
+}
+
+function renderPage(pageNumber){
+
+    let selectedArticles = sliceArray(allElementsOfCategory, pageNumber);
+
+    let html = "";
+
+    selectedArticles.forEach(function (articulo) {
+
+        html += "<article class='col-12-4' data-category=" + articulo.categorias + " >";
+        html += "<div class='box-interna'>";
+        html += "<div class='figure'>";
+        html += "<img src=" + articulo.urlImagen + ">";
+        html += "</div>";
+        html += "<div class='description'>";
+        html += "<div class='text'>";
+        html += "<h3>" + articulo.titulo + "</h3>";
+        html += "<p>" + articulo.subTitulo + "</p>";
+        html += "</div>";
+        html += "<div class='action'>";
+        html += "<a href='" + articulo.urlEnlace + "'>" + articulo.textoEnlace + "</a>";
+        html += "</div>";
+        html += "</div >";
+        html += "</div > ";
+        html += "</article >";
+
+    });
+
+    elementos.contendorNotas.innerHTML = html;
+}
+
+// Slice Array
+function sliceArray(articulos, from) {    
+    return articulos.slice((from - 1) * CANT_ARTICLES_PER_PAGE, from * CANT_ARTICLES_PER_PAGE);
+}
+
+// Events
+$(elementos.filter).on("click", "li a", function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    let $value = $(this).data("id");
+
+    $(elementos.filterSeleccionadoSpan).text($(this).text());
+    $(this).closest('ul').removeClass("show");
+
+    setArticles($value);
+    renderPage(1);
+    renderPaginator();   
+    hideArrows(1); 
+});
+
+$(elementos.filterSeleccionado).on("click", function (e) {
+    e.stopPropagation();
+    $(".filtro").toggleClass("show");
+});
+
+$(elementos.paginador).on("click", "[data-number]", function (e) {
+    e.preventDefault();
+    let $this = $(this);
+    let fromNumber = parseInt($this.data("number"));
+    activatePaginatorItem(fromNumber);
+});
+
+$(elementos.paginador).on("click", "[data-comp=prev]", function (e) {
+    e.preventDefault();
+    let activeNumber = numberActiveBttn() - 1;    
+    activatePaginatorItem(activeNumber);
+});
+
+$(elementos.paginador).on("click", "[data-comp=next]", function (e) {
+    e.preventDefault();
+    let activeNumber = numberActiveBttn() + 1;
+    activatePaginatorItem(activeNumber);
+});
+
+
+function numberActiveBttn() {
+    let res;
+    $(elementos.bttnContainer).find("a").each(function (index) {
+        var $elem = $(this);
+        if ($elem.hasClass("active")) {
+            res = $elem.data("number");
+        };
+    });
+    return res;
+}
+
+function hideArrows(fromNumber) {
+    let cantElementsOfCategory = allElementsOfCategory.length;
+    fromNumber >= Math.ceil(cantElementsOfCategory / CANT_ARTICLES_PER_PAGE) ? $("[data-comp=next]").hide() : $("[data-comp=next]").show();
+    fromNumber <= 1 ? $("[data-comp=prev]").hide() : $("[data-comp=prev]").show();
+}
+
+function activatePaginatorItem(activeNumber){
+    renderPage(activeNumber);
+    $("[data-number]").removeClass("active");
+    $("[data-number=" + activeNumber + "]").addClass("active");
+    hideArrows(activeNumber);
+}
+
+init();
